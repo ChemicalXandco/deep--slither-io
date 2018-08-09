ab27894b442e3a07e935fa8df910974f3f380d8f
ChemicalXandco <jimjoh41@gmail.com>
2018-08-09 18:01:09 +0100

    More GUI and inferstructure

--- GUI.py
+++ GUI.py
@@ -2,6 +2,17 @@ from tkinter import *
 from PIL import Image
 from PIL import ImageTk
 from deeptools import deeptool
+import numpy as np
+
+class dummydt:
+    def __init__(self):
+        self.notfake = False
+    class ff:
+        def getopencvformat():
+            print("None of this!")
+
+dt = dummydt()
+working = False
 
 root = Tk()
 root.title('.IO Bot')
@@ -13,25 +24,67 @@ mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
 mainframe.columnconfigure(0, weight = 1)
 mainframe.rowconfigure(0, weight = 1)
 
+def start():
+    global working
+    working = True
+    dt.pause = False
+
+def stop():
+    global working
+    working = False
+
+def cap(captime, button, root):
+    try:
+        timetocap = int(captime)
+    except Exception as e:
+        print(e)
+        return
+    dt.capdata(timetocap, False, button, root)
+    button.config(text="Capture data")
+    root.update_idletasks()
+
+def loadnet():
+    global dt
+    dt = deeptool(0, 'test')
+    dt.model('y')
+
+trainit = Button(root, text="Train (May take a long time)", command=lambda: dt.train(1))
+trainit.grid(column=1,row=5)
+
+run = Button(root, text="Start bot", command=start)
+run.grid(column=1,row=4)
+
+stoprun = Button(root, text="Stop bot", command=stop)
+stoprun.grid(column=1,row=5)
+
+docap = Button(root, text="Capture data", command=lambda: cap(entrycap.get(), docap, root))
+docap.grid(column=1,row=2)
+
+entrycap = Entry(root)
+entrycap.grid(column=1,row=1)
+
+entrycaplab = Label(root, text="Time to capture: ")
+entrycaplab.grid(column=0,row=1)
+
+pauselab = Label(root, text="Press 'P' to pause bot when running \npress again to unpause")
+pauselab.grid(column=1,row=0)
+
 res = (root.winfo_screenwidth(), root.winfo_screenheight())
 
-panel = None
+black = np.zeros((int(res[1]/4), int(res[0]/4), 3))
 
-dt = deeptool(0, 'slither')
-
-dt.model('y')
+screen = Image.fromarray(black.astype(np.uint8))
+tkscreen = ImageTk.PhotoImage(screen)
+panel = Label(image=tkscreen)
+panel.image = tkscreen
+panel.grid(column=0,row=0)
 
 while True:
-    dt.control()
+    if working and dt.notfake:
+        dt.control()
 
-    screen = Image.fromarray(dt.ff.getopencvformat(dt.img, True, int(res[0]/4), int(res[1]/4)))
-    tkscreen = ImageTk.PhotoImage(screen)
-
-    if panel is None:
-        panel = Label(image=tkscreen)
-        panel.image = tkscreen
-        panel.grid(column=0,row=0)
-    else:
+        screen = Image.fromarray(dt.ff.getopencvformat(dt.img, True, int(res[0]/4), int(res[1]/4)))
+        tkscreen = ImageTk.PhotoImage(screen)
         panel.configure(image=tkscreen)
         panel.image = tkscreen
 
--- deeptools.py
+++ deeptools.py
@@ -7,43 +7,84 @@ from alexnet import alexnet
 class deeptool:
     def __init__(self, captures, modelname):
         self.ff = FrameFunc()
+        self.np = np
+        self.notfake = True
         # time.sleep(5)
         self.start = True # time.time()
         self.pause = False
 
         self.modelname = modelname
+
+        self.imgs, self.avgdirs = [], []
+
+        if captures > 0:
+            self.capdata(captures)
+
+    def capdata(self, captures, printit=True, gui=None, root=None):
         imgs = []
         avgdirs = []
         avgspaces = []
-        k = None
-        print('\ncapturing data')
+        timer = captures+1
+        for i in range(5):
+            timer = 5-i
+            if printit:
+                print("Starting capture in %d" % timer)
+            if gui != None:
+                gui.config(text="Starting capture in %d" % timer)
+                root.update_idletasks()
+            time.sleep(1)
+        timer = captures+1
+        if printit:
+            print('\ncapturing data')
         for i in range(captures*10):
             self.img, avgdir, avgspace = self.ff.getcap()
             imgs.append(self.img)
             avgdirs.append(avgdir)
             avgspaces.append(avgspace)
-        self.imgs = np.expand_dims(np.array(imgs), axis=3)#.astype('float32')
+            if timer != round(captures-(i/10)):
+                timer = round(captures-(i/10))
+                if printit:
+                    print(str(timer))
+                if gui != None:
+                    gui.config(text=str(timer))
+                    root.update_idletasks()
+                    root.update()
+        self.imgs = np.expand_dims(np.array(imgs), axis=3)
         avgdirz = []
         for i in avgdirs:
             avgdirz.append((round((i*10)))/10)
-        # avgdirs = np.expand_dims(np.array(avgdirz), axis=1)#.astype('float32')
         self.avgdirs = []
         for i, j in zip(avgdirz, avgspaces):
             self.avgdirs.append(np.concatenate((getdummy(i), getbindummy(j))))
-        # print(str(avgdirs))
-        # quit()
+        if printit:
+            print('captured data')
 
-        print('captured data')
-
-    def model(self, load, n_epoch=100):
+    def model(self, load, n_epoch=0):
         self.model = alexnet(72, 128, 0.01, 13)
 
         if load != 'y':
-            # Training
-            self.model.fit(self.imgs, self.avgdirs, n_epoch, batch_size=100, show_metric=True)
-            self.model.save('%s.tfl' % self.modelname)
+            if n_epoch >= 1:
+                self.train(n_epoch)
+            else:
+                self.savemodel()
         else:
             self.model.load('%s.tfl' % self.modelname)
+
+    def train(self, n_epoch):
+        try:
+            a = int(n_epoch)
+        except ValueError as e:
+            print(e)
+        if self.imgs == [] and self.avgdirs == []:
+            print("Can't train on nothing!")
+        elif a != n_epoch:
+            print("Number of epochs is not int!")
+        else:
+            self.model.fit(self.imgs, self.avgdirs, n_epoch, batch_size=100, show_metric=True)
+            self.savemodel()
+
+    def savemodel(self):
+        self.model.save('%s.tfl' % self.modelname)
 
     def control(self, showcv=False):
         if not self.pause and self.ff.capavailable():
@@ -77,8 +118,7 @@ if __name__ == '__main__':
     captures = int(input('How many seconds of capture? '))
     modelname = input('Name of model? ')
     dt = deeptool(captures, modelname)
-    load = input('Load pretrained model? ')
-    dt.model(load)
+    dt.model('y', 100)
     while True:
         dt.control(True)
 
